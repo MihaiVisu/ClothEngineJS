6dbd4c2c50a72de8a29150d3dc0707f4beab844e
mihaivisu <mihaivisuian@gmail.com>
2015-08-29 17:18:18 +0100

    add js configuration file

---
+++ js/Cloth/config.js
@@ -0,0 +1,13 @@
+var Cloth = window.Cloth || {};
+
+// settings
+
+var physics_accuracy  = 3,
+    mouse_influence   = 20,
+    mouse_cut         = 5,
+    gravity           = 1200,
+    cloth_height      = 30,
+    cloth_width       = 50,
+    start_y           = 20,
+    spacing           = 7,
+    tear_distance     = 60;

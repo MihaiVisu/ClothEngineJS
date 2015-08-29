8ad4078e405e2db38ef80c8281976c71747a7774
mihaivisu <mihaivisuian@gmail.com>
2015-08-29 18:34:06 +0100

    add point.js that includes point prototype

---
+++ js/Cloth/point.js
@@ -0,0 +1,50 @@
+var Cloth = Cloth || {};
+
+Cloth.Point = function (x, y) {
+    'use strict';
+
+    this.x      = x;
+    this.y      = y;
+    this.px     = x;
+    this.py     = y;
+    this.vx     = 0;
+    this.vy     = 0;
+    this.pin_x  = null;
+    this.pin_y  = null;
+
+    this.constraints = [];
+};
+
+Cloth.Point.prototype.update = function (delta) {
+    'use strict';
+
+    if (mouse.down) {
+
+        var diff_x = this.x - mouse.x,
+            diff_y = this.y - mouse.y,
+            dist = Math.sqrt(diff_x * diff_x + diff_y * diff_y);
+
+        if (mouse.button === 1) {
+
+            if (dist < mouse_influence) {
+                this.px = this.x - (mouse.x - mouse.px) * 1.8;
+                this.py = this.y - (mouse.y - mouse.py) * 1.8;
+            }
+
+        } else if (dist < mouse_cut) this.constraints = [];
+    }
+
+    this.add_force(0, gravity);
+
+    delta *= delta;
+    nx = this.x + ((this.x - this.px) * .99) + ((this.vx / 2) * delta);
+    ny = this.y + ((this.y - this.py) * .99) + ((this.vy / 2) * delta);
+
+    this.px = this.x;
+    this.py = this.y;
+
+    this.x = nx;
+    this.y = ny;
+
+    this.vy = this.vx = 0;
+};

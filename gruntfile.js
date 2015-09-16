0e409290bf873e78631976d827c36fc7f24a2139
mihaivisu <mihaivisuian@gmail.com>
2015-09-16 17:52:09 +0100

    add new interface

--- gruntfile.js
+++ gruntfile.js
@@ -33,7 +33,8 @@ module.exports = function (grunt) {
             libs: {
                 files: {
                     'generated/js/project_libs.min.js': [
-                        'node_modules/jquery/dist/jquery.min.js'
+                        'node_modules/jquery/dist/jquery.min.js',
+                        'node_modules/bootstrap/dist/js/bootstrap.min.js'
                     ]
                 }
             }
@@ -48,19 +49,42 @@ module.exports = function (grunt) {
                     new (require('less-plugin-clean-css'))
                 ]
             },
-            target: {
+            gui: {
                 files: {
                     'generated/css/project_gui.min.css': [
                         'less/*.less'
                     ]
                 }
+            },
+            libs: {
+                files: {
+                    'generated/css/project_libs.min.css': [
+                        'node_modules/bootstrap/less/bootstrap.less'
+                    ]
+                }
             }
         },
+        imagemin: {
+            fonts: {
+                options: {
+                    optimizationLevel: 3,
+                    svgoPlugins: [{ removeViewBox: false }]
+                },
+                files: {
+                    'generated/fonts/glyphicons-halflings-regular.eot': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.eot',
+                    'generated/fonts/glyphicons-halflings-regular.svg': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.svg',
+                    'generated/fonts/glyphicons-halflings-regular.ttf': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.ttf',
+                    'generated/fonts/glyphicons-halflings-regular.woff': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff',
+                    'generated/fonts/glyphicons-halflings-regular.woff2': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff2',
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     // Default task(s).
     grunt.registerTask('default', ['watch']);

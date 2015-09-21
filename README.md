7271318d8558f2260cd0c470db8dce8c3bfd0b4a
mihaivisu <mihaivisuian@gmail.com>
2015-09-21 20:05:57 +0100

    new git

---
+++ README.md
@@ -0,0 +1,3 @@
+# ClothEngineJS
+This is my first version of a tearable cloth engine built using javascript and HTML5 canvas + a bit of bootstrap for the interface.
+The javascript files are minified in one file and the less files are compiled via the Gruntfile in the project.
---
+++ gruntfile.js
@@ -0,0 +1,92 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            options: {
+                spawn: false
+            },
+            scripts: {
+                files: [
+                    'js/*.js',
+                    'js/ClothEngine/*.js'
+                ],
+                tasks: ['uglify:gui']
+            },
+            styles: {
+                files: ['less/*.less'],
+                tasks: ['less']
+            }
+        },
+        uglify: {
+            options: {
+                beautify: false
+            },
+            gui: {
+                files: {
+                    'generated/js/project_gui.min.js':[
+                        'js/ClothEngine/cloth.js',
+                    ]
+                }
+            },
+            libs: {
+                files: {
+                    'generated/js/project_libs.min.js': [
+                        'node_modules/jquery/dist/jquery.min.js',
+                        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+                    ]
+                }
+            }
+        },
+        less: {
+            options: {
+                beautify: false,
+                plugins: [
+                    new (require('less-plugin-autoprefix'))({
+                        browsers: ["last 2 versions"]
+                    }),
+                    new (require('less-plugin-clean-css'))
+                ]
+            },
+            gui: {
+                files: {
+                    'generated/css/project_gui.min.css': [
+                        'less/*.less'
+                    ]
+                }
+            },
+            libs: {
+                files: {
+                    'generated/css/project_libs.min.css': [
+                        'node_modules/bootstrap/less/bootstrap.less'
+                    ]
+                }
+            }
+        },
+        imagemin: {
+            fonts: {
+                options: {
+                    optimizationLevel: 3,
+                    svgoPlugins: [{ removeViewBox: false }]
+                },
+                files: {
+                    'generated/fonts/glyphicons-halflings-regular.eot': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.eot',
+                    'generated/fonts/glyphicons-halflings-regular.svg': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.svg',
+                    'generated/fonts/glyphicons-halflings-regular.ttf': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.ttf',
+                    'generated/fonts/glyphicons-halflings-regular.woff': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff',
+                    'generated/fonts/glyphicons-halflings-regular.woff2': 'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff2',
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+
+};
